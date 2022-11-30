54194e56b5bae2198901cf15747d3c8d4a3cdd2f
Jaderson Silva Araújo <117588427+sajaderson@users.noreply.github.com>
2022-11-29 21:52:25 -0300

    Atualização na responsividade
    - Deixando as divs da sessão flexíveis;

--- script/menu-responsivo.js
+++ script/menu-responsivo.js
@@ -1,9 +1,16 @@
+var elemento = document.getElementsByClassName('conteudo-services')[0]
+var propriedade = window.getComputedStyle(elemento).getPropertyValue('width')
+var divult = document.querySelector('.conteudo-services:last-child')
+divult.style.flexGrow = 0
+divult.style.flexBasis = 'auto';
+divult.style.width = propriedade
+
 function menuResponsivo(){
     var menu = document.querySelector('.menu')
     menu.classList.toggle('active')
 }
 
-var bt = document.getElementById('bt-esq')
+/* var bt = document.getElementById('bt-esq')
 bt.addEventListener('click', function(){
     var slideShow = document.getElementById('works-slide')
     slideShow.scrollBy(-300, 0)
@@ -13,4 +20,4 @@ var bt = document.getElementById('bt-dir')
 bt.addEventListener('click', function(){
     var slideShow = document.getElementById('works-slide')
     slideShow.scrollBy(328, 0)
-})
+}) */
