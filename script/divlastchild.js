b05f3aa5982ebce4f6ba19be991dc8db4c138d24
sajaderson <117588427+sajaderson@users.noreply.github.com>
2022-11-30 10:10:35 -0300

    Melhorias na responsividade
    - Criação do script para ajustar o ultimo parágrafo da sessão quando houvesse um redimensionamento da janela ou alteração no zoom.

---
+++ script/divlastchild.js
@@ -0,0 +1,22 @@
+var elemento = document.getElementsByClassName('conteudo-services')[0]
+var propriedadeinicial = window.getComputedStyle(elemento).getPropertyValue('width')
+var divlastchild = document.querySelector('.conteudo-services:last-child')
+divlastchild.style.flexGrow = 0
+divlastchild.style.flexBasis = 'auto';
+divlastchild.style.width = propriedadeinicial
+
+var body = document.body
+var largurainial = body.clientWidth
+
+window.addEventListener('resize', function(){
+    var propriedaderesize = window.getComputedStyle(elemento).getPropertyValue('width')
+    divlastchild.style.width = propriedaderesize
+    
+    //var browserZoomLevel = Math.round(window.devicePixelRatio * 100); // mostra o nivel do Zoom
+    body = document.body
+    var larguraresize = body.clientWidth
+
+    if(larguraresize == largurainial){
+        divlastchild.style.width = propriedadeinicial
+    }
+})
--- script/menu-responsivo.js
+++ script/menu-responsivo.js
@@ -1,10 +1,3 @@
-var elemento = document.getElementsByClassName('conteudo-services')[0]
-var propriedade = window.getComputedStyle(elemento).getPropertyValue('width')
-var divult = document.querySelector('.conteudo-services:last-child')
-divult.style.flexGrow = 0
-divult.style.flexBasis = 'auto';
-divult.style.width = propriedade
-
 function menuResponsivo(){
     var menu = document.querySelector('.menu')
     menu.classList.toggle('active')
