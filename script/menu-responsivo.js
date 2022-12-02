4082f8814ee09a3438150a124248439e2e68d0f8
Jaderson Silva Araújo <117588427+sajaderson@users.noreply.github.com>
2022-12-01 21:41:55 -0300

    Atualizações
    - Script para responsividade;
    - Slide de Imagens;

--- script/menu-responsivo.js
+++ script/menu-responsivo.js
@@ -2,15 +2,3 @@ function menuResponsivo(){
     var menu = document.querySelector('.menu')
     menu.classList.toggle('active')
 }
-
-/* var bt = document.getElementById('bt-esq')
-bt.addEventListener('click', function(){
-    var slideShow = document.getElementById('works-slide')
-    slideShow.scrollBy(-300, 0)
-})
-
-var bt = document.getElementById('bt-dir')
-bt.addEventListener('click', function(){
-    var slideShow = document.getElementById('works-slide')
-    slideShow.scrollBy(328, 0)
-}) */
--- script/sessao-works.js
+++ script/sessao-works.js
@@ -1,15 +1,36 @@
-var larguraImagens = function (event){
-    var imagens = document.getElementsByClassName('works-imagens')
-    var sessao = document.getElementById('section-works')
-    var lsessao = parseFloat(window.getComputedStyle(sessao).getPropertyValue('width'))
+/* #### Aplicando a mesma largura e altura das divs da sessão Services anterior na divs da sessão Works, para manter o alinhamento. #### */
 
-    console.log(lsessao)
-    lsessao = (lsessao - 75)/4
-    console.log(lsessao)
+var larguraImagens = function (event){
+    var elementoRefServices = document.getElementById('divI')
+    var larguradivI = window.getComputedStyle(elementoRefServices).getPropertyValue('width')
+    var alturadivI = parseFloat(window.getComputedStyle(elementoRefServices).getPropertyValue('height'))
+    var imagens = document.getElementsByClassName('works-imagens')    
+    var elementoRefWorks = document.getElementById('conteudo-works')
+    var elementoEsconder = document.getElementById('esconderBarra')
+
+    elementoEsconder.style.height = `${alturadivI - 61}px` // 61 é o tamanho do botão com margin e padding.
+    elementoRefWorks.style.width = larguradivI
+    elementoRefWorks.style.minWidth = larguradivI
 
     for(var i = 0; i < imagens.length; i++){
-        imagens[i].style.width = `${lsessao}px`
+        imagens[i].style.width = larguradivI
+        imagens[i].style.height = `${alturadivI - 61}px` // 61 é o tamanho do botão com margin e padding.
     }
 }
 window.addEventListener('load', larguraImagens, false)
 window.addEventListener('resize', larguraImagens, false)
+
+
+/* ##################### Ação de passar a imagens ################# */
+
+var bt = document.getElementById('bt-esq')
+bt.addEventListener('click', function(){
+    var slideShow = document.getElementById('works-slide')
+    slideShow.scrollBy(-337.5, 0)
+})
+
+var bt = document.getElementById('bt-dir')
+bt.addEventListener('click', function(){
+    var slideShow = document.getElementById('works-slide')
+    slideShow.scrollBy(337.5, 0)
+})
